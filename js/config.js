f7a05d50a72b02ed4e83a4dd7523e46205851fc2
Shanikah-Boyce <boyceshanikah@gmail.com>
2025-03-18 13:41:36 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qosg4ie69e.execute-api.us-west-1.amazonaws.com/dev' 
     }
 };
